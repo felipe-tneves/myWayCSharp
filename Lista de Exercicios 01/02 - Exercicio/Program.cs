830fa77e4c24c93f36e83996e7c37c80628a5486
Felipe Tadeu <felipe.tadeusantos1@gmail.com>
2021-10-25 12:29:42 -0300

    Lista de exercicios, exercicios 1 e 2

---
+++ Lista de Exercicios 01/02 - Exercicio/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02___Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2 – Crie um algoritmo que calcule a área de um retângulo.
+
+            Console.WriteLine("Exercicio 02");
+
+            double result;
+
+            Console.WriteLine("Informe a altura do retângulo: ");
+            double altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe a largura do retângulo: ");
+            double largura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o número para a opção de calculo: ");
+            Console.WriteLine("1 - Area;\n" +
+                "2 - Perimetro\n" +
+                "0 - Sair");
+            int escolha = Int32.Parse(Console.ReadLine());
+
+            switch (escolha)
+            {
+                case 1:
+                    result = altura * largura;
+                    Console.WriteLine("O resultado da área é: " + result);
+                    break;
+
+                case 2:
+                    result = (2 * altura) + (2 * largura);
+                    Console.WriteLine("O resultado do perimetro é: " + result);
+                    break;
+
+                default:
+                    break;
+            }
+
+
+            Console.WriteLine("Tecle enter para sair...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lista de Exercicios 01/Exercicios 01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01___Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 – Crie um algoritmo que exiba a tabuada de um número.
+            Console.WriteLine("Executando o exercicio 01");
+
+            //1 X 2 = 2
+
+            //int valorDaTaboada = 9;
+            int result;
+
+            Console.WriteLine("Digite o valor para ver a taboada: ");
+            int valorDaTaboada = Int32.Parse(Console.ReadLine());
+
+
+            for (int i = 1; i <= 10; i++)
+            {
+                result = i * valorDaTaboada;
+                Console.WriteLine(i + " X " + valorDaTaboada + " = " + result);
+            }
+
+
+            Console.WriteLine("Tecle enter para sair...");
+            Console.ReadLine();
+        }
+    }
+}
